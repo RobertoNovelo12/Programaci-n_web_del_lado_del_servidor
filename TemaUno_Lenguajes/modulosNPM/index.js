9e1f26fe184c3f61a9c83269c51cd765b9364283
RobertoNovelo12 <robertonovelo98@gmail.com>
2026-02-25 01:19:18 -0500

    Ya hay superheores

--- TemaUno_Lenguajes/modulosNPM/index.js
+++ TemaUno_Lenguajes/modulosNPM/index.js
@@ -1,5 +1,8 @@
 import generateName from 'sillyname';
+import {randomSuperhero}  from 'superheroes';
 
+var heroe = randomSuperhero();
 var sillyName = generateName();
 
 console.log(sillyName)
+console.log(heroe);
