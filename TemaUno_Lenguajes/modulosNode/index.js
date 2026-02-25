77f5ee5cc1fa9106db400e2564ab3050778ce348
RobertoNovelo12 <robertonovelo98@gmail.com>
2026-02-25 01:02:25 -0500

    Ejercicio que crea y lee txt

--- TemaUno_Lenguajes/modulosNode/index.js
+++ TemaUno_Lenguajes/modulosNode/index.js
@@ -4,3 +4,8 @@ fs.writeFile('archivo.txt', 'Hola desde NodeJS', (err) => {
     if (err) throw err;
     console.log('El archivo ha sido creado con éxito.');
 });
+
+fs.readFile ('./archivo.txt','utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
