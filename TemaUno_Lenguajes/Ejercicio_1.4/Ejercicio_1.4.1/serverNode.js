30169ab7ba65778538f215a09b82f3a71b89fecb
RobertoNovelo12 <robertonovelo98@gmail.com>
2026-02-26 22:35:16 -0500

    Ejercicios resueltos y gitignore debdería estár funcinando

---
+++ TemaUno_Lenguajes/Ejercicio_1.4/Ejercicio_1.4.1/serverNode.js
@@ -0,0 +1,27 @@
+// Jorge Roberto Novelo Poot - 8B
+// Ejemplo de aplicación en servidor web ServerNode.js
+
+import http from 'http';
+import url from 'url';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+
+    if(parsedUrl.pathname === '/' && req.method === 'GET') {
+       res.writeHead(200, { 'Content-type': 'text/html;charset=utf-8'});
+       res.end('<h1>Bienvenido a mi página web</h1>');
+    } else if (parsedUrl.pathname === '/about' && req.method === 'GET') {
+       res.writeHead(200, { 'Content-type': 'text/html;charset=utf-8'});
+       res.end('<h1>Acerca de</h1>');
+    } else {
+        res.writeHead(404, { 'content-Type': 'text/html'});
+        res.end('<h1>404 Not Found</h1>');
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Servidor corriendo en http://${hostname}:${port}/`)
+});
